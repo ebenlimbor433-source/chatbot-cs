ab9a589d98f412374b00f081adc64ca0e90fd802
ebenlimbor433-source <ebenlimbor433@gmail.com>
2026-08-04 22:44:26 +1200

    Fix base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/chatbot-cs/', // Matches repository name
-export default defineConfig({
-  base: '/your-repository-name/',
-  plugins: [react()],
+  base: '/chatbot-cs/',
 })
-
-
